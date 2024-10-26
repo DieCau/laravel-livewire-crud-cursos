ff97a122c97c996f698573ce0fef153ee27f9aa7
Diego Caucota <diegocaucota@gmail.com>
2024-10-26 11:16:03 -0300

    add: edit-curso component

--- app/Livewire/Cursos.php
+++ app/Livewire/Cursos.php
@@ -2,10 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Curso;
 use Livewire\Component;
 
 class Cursos extends Component
 {
+    public $cursos;
+    public function mount()
+    {
+        $this->showCurso();
+    }
+
+    public function showCurso()
+    {
+        $this->cursos = Curso::all();
+    }
+
     public function render()
     {
         return view('livewire.cursos');
--- resources/views/livewire/cursos.blade.php
+++ resources/views/livewire/cursos.blade.php
@@ -41,26 +41,39 @@
                     </tr>
                 </thead>
                 <tbody>
+
+                    @forelse ($cursos as $index=>$curso)
                     <tr
                         class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                         <th scope="row"
                             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                            {{ $index+1 }}
                         </th>
                         <td class="px-6 py-4">
-                            Silver
+                            {{ $curso->name_curso }}
                         </td>
                         <td class="px-6 py-4">
-                            Laptop
+                            {{ $curso->description_curso }}
                         </td>
                         <td class="px-6 py-4">
-                            $2999
+                            {{ $curso->price_curso }}
                         </td>
                         <td class="px-6 py-4">
                             <a href="#"
-                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+
+                            <button type="button" class="focus:outline-none text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mb-2 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">Eliminar</button>
                         </td>
                     </tr>
+
+                    @empty
+
+                    @endforelse
+
+
+
+
+
 
                 </tbody>
             </table>
