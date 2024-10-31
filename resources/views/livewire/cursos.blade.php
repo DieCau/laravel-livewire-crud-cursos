8dd3b0fa4b3425ecdde69aa52af487a1a29f6766
Diego Caucota <diegocaucota@gmail.com>
2024-10-30 23:47:20 -0300

    add: sweetAlert.js component

--- resources/views/livewire/cursos.blade.php
+++ resources/views/livewire/cursos.blade.php
@@ -62,23 +62,14 @@
                             <button type="button" wire:navigate href="{{ route('curso.edit', $curso) }}"
                                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</button>
 
-                            <button type="button" class="focus:outline-none text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mb-2 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">Eliminar</button>
+                            <button type="button" class="focus:outline-none text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mb-2 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900" wire:click="confirmDelete()">Eliminar</button>
                         </td>
                     </tr>
-
                     @empty
-
                     @endforelse
-
-
-
-
-
-
                 </tbody>
             </table>
         </div>
-
     </div>
-
 </div>
+
